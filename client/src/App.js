7e86a496af92a475f792329b0dee63c300e87c50
Bhargav Patel <patelbhargav932002@gmail.com>
2022-03-15 15:31:26 +0530

    UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,21 @@
-import { useSelector } from 'react-redux';
+// routes
+import Router from './routes';
+// theme
+import ThemeConfig from './theme';
+import GlobalStyles from './theme/globalStyles';
+// components
+import ScrollToTop from './components/ScrollToTop';
+import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
 
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, StyledEngineProvider } from '@mui/material';
+// ----------------------------------------------------------------------
 
-// routing
-import Routes from 'routes';
-
-// defaultTheme
-import themes from 'themes';
-
-// project imports
-import NavigationScroll from 'layout/NavigationScroll';
-
-// ==============================|| APP ||============================== //
-
-const App = () => {
-    const customization = useSelector((state) => state.customization);
-
-    return (
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={themes(customization)}>
-                <CssBaseline />
-                <NavigationScroll>
-                    <Routes />
-                </NavigationScroll>
-            </ThemeProvider>
-        </StyledEngineProvider>
-    );
-};
-
-export default App;
+export default function App() {
+  return (
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeConfig>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,30 @@
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
 import ReactDOM from 'react-dom';
-
-// third party
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 
-// project imports
-import * as serviceWorker from 'serviceWorker';
-import App from 'App';
-import { store } from 'store';
+//
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-// style + assets
-import 'assets/scss/style.scss';
-
-// ==============================|| REACT DOM RENDER  ||============================== //
+// ----------------------------------------------------------------------
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>,
+  document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// If you want to enable client cache, register instead.
 serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
